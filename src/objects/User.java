b10fc16e40de80296a0add3948b7cfc2fd187fe7
petroUshtan <petro.ushtan@yandex.ua>
2017-04-24 00:46:22 +0300

    Delete entity folder

--- src/objects/User.java
+++ src/objects/User.java
@@ -1,12 +1,29 @@
 package objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Work on 12.04.2017.
  */
+
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "status")
     private String status;
+
+    @Column(name = "password")
     private String password;
+
+    @Id
+    @Column(name = "id")
     private Long id;
 
     public User(String username, String password, String status) {
@@ -63,6 +80,7 @@ public class User {
     public String toString() {
         return "User{" +
                 "username='" + username + '\'' +
+                ", status='" + status + '\'' +
                 ", password='" + password + '\'' +
                 ", id=" + id +
                 '}';
